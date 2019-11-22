80c8d54acc18a9e8a667efe9a68befab11968751
Matthew Honour <matthewpeterhonour@gmail.com>
2019-11-22 20:10:50 +0000

    Wrote a program that checks if a word is a palindrome

---
+++ palindrome-checker.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+bool doesContainPalindrome(char word[], int size){
+	for(int i =  0; i< size; i++){
+		char leftLetter = word[i];
+		char rightLetter = word[(size-1)-i];
+		if (leftLetter!= rightLetter){
+			return false;
+		}
+		// palindromes are symetrical so you don't need to loop through the entire word
+		if(i > size/2)break;
+	}
+	return true;
+}
+
+int main(){
+	char word[] =  "racecar";
+	bool result = doesContainPalindrome(word, sizeof(word)-1);
+	if(result == true){
+		cout << word <<   "  -> Is a Palindrome :)" << endl;
+	}
+	else{
+		cout << word <<   "  -> Is NOT a Palindrome >:(" << endl;
+	}
+	return 0;
+}
